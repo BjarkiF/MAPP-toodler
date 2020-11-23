fb6b02f69d2d712b7475c9612e305e975dae98ee
BjarkiF <54940539+BjarkiF@users.noreply.github.com>
2020-11-23 17:05:46 +0000

    Initial commit

---
+++ toodler/src/views/main/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Main = () => (
+  <View>
+    <Text>Welcome to The Toodler!</Text>
+  </View>
+);
+
+export default Main;
