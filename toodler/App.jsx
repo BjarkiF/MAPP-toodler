da21d3771989bef5bbcf5358894bdfdb668051cf
Einar Agust <einaragusta@gmail.com>
2020-11-28 00:19:14 +0000

    before pull

--- toodler/App.jsx
+++ toodler/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import AppContainer from './src/routes';
 
+import ErrorBoundaryClass from './src/services/ErrorBondaryClass';
+
 export default function App() {
   return (
-    <AppContainer />
+    <ErrorBoundaryClass>
+      <AppContainer />
+    </ErrorBoundaryClass>
   );
 }
