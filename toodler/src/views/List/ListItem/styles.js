98011a39b48ef5370dff147147e66e0a0710fa6e
Einar Agust <einaragusta@gmail.com>
2020-11-28 02:53:58 +0000

    'gleymdi'

---
+++ toodler/src/views/List/ListItem/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+// const {width} = useWindowDimensions();
+// const {height} = useWindowDimensions();
+
+const styles = StyleSheet.create({
+  listName: {
+    padding: 15,
+  },
+});
+
+export default styles;
